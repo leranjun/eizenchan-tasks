545e5e307b7d967bb1d17085e33fe083a835d03f
Frank <25704248+leranjun@users.noreply.github.com>
2022-07-02 18:22:35 +0800

    feat(mooncake): add mooncake.sh

--- manual/mooncake/mooncake.sh
+++ manual/mooncake/mooncake.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-toolforge-jobs run mooncake --command "python3 mooncake.py" --image tf-bullseye-std --wait
+toolforge-jobs run mooncake --command "python3 mooncake.py" --image tf-python39 --wait
 cat mooncake.out
